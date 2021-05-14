88e1f4e5a4a11c6eb1e9762b0aa34748cd5b6847
Jan Ossolinski <ossolinski.jan@gmail.com>
2021-05-14 14:14:57 +0000

    Add NotFound view

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
-import clsx from 'clsx';
-
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+// import clsx from 'clsx';
 
 import styles from './NotFound.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>NotFound</h2>
-    {children}
+const Component = () => (
+  <div className={styles.root}>
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <h2>Oops!<br></br> Our dog ate this page!</h2>
+        <Button component={ Link } to={'/'} variant="contained" color="primary" className={styles.button}>Homepage</Button>
+      </div>
+      <img src="/images/notfound.png" alt='Logo' title='Oops!' className={styles.image}/>
+      {/* <h1>Oops! Our dog ate this page!</h1>
+      <Button component={ Link } to={'/'} variant="contained" color="primary" className={styles.button}>Go back to Homepage</Button> */}
+    </div>
   </div>
 );
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
 };
-
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   Component as NotFound,
--- src/components/views/ProductPage/ProductPage.test.js
+++ src/components/views/ProductPage/ProductPage.test.js
@@ -4,7 +4,15 @@ import { ProductPageComponent } from './ProductPage';
 
 describe('Component ProductPage', () => {
   it('should render without crashing', () => {
-    const component = shallow(<ProductPageComponent />);
+    const product = {
+      id: '3123123',
+      category: 'Vitamins & Supplements',
+      title: 'Joint for Dogs',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+      price: 135,
+      image: ['/images/products/vitamins1.jpg', '/images/products/vitamins2.jpg', '/images/products/vitamins3.jpg'],
+    };
+    const component = shallow(<ProductPageComponent product={product}/>);
     expect(component).toBeTruthy();
   });
 });
