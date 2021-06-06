38022eadb365844579cc70510777b58d64951de3
Jan Ossolinski <ossolinski.jan@gmail.com>
2021-06-06 19:44:03 +0000

    Modify Readme.md & add favicon

--- public/sw.js
+++ public/sw.js
@@ -36,8 +36,8 @@ self.addEventListener('activate',  event => {
 });
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches
-      .match(event.request, { ignoreSearch:true })
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
